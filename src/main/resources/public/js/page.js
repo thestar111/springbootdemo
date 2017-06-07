81578ec3013e7c39a620b6b3df38bcafb71da7c8
HidalgoWolves <zhouping19911013@163.com>
2017-06-07 15:19:16 +0800

    添加fullPage

---
+++ src/main/resources/public/js/page.js
@@ -0,0 +1,9 @@
+$(function() {
+    $("#dowebok").fullpage({
+        verticalCentered: true,
+        navigation: true,
+        navigationPosition: 'right',
+        anchors: ['page1', 'page2', 'page3', 'page4'],
+        slidesNavigation: true
+    });
+});
